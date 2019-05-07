8a195f4c5f711f3f2bb5df4969e4c09aa3e86ca4
nickylau82 <70458055@qq.com>
2019-05-07 16:25:31 +0800

    :construction: complete static server & dingtalk msg

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const puppeteer = require('puppeteer')
+const Koa = require('koa2')
+const router = require('koa-router')()
+const Excel = require('exceljs')
+const axios = require('axios')
+const path = require('path')
+const KoaStatic = require('koa-static')
+
+const app = new Koa()
+
 let enList = []
-const app = async () => {
+const task = async () => {
   console.log('loading....')
   const browser = await (puppeteer.launch({
   //设置超时时间
@@ -105,6 +114,63 @@ const app = async () => {
   console.log(enList)
   console.log('loading done....')
   browser.close()
+  const filename = `kcb-${new Date() - 0}.xlsx`
+  var workbook = new Excel.stream.xlsx.WorkbookWriter({
+    filename: `./data/${filename}`
+  });
+  var worksheet = workbook.addWorksheet('Sheet');
+
+  worksheet.columns = [
+    { header: '序号', key: '序号' },
+    { header: '发行人全称', key: '发行人全称' },
+    { header: '发行人全称:href', key: '发行人全称:href' },
+    { header: '审核状态', key: '审核状态' },
+    { header: '注册地', key: '注册地' },
+    { header: '证监会行业', key: '证监会行业' },
+    { header: '保荐机构', key: '保荐机构' },
+    { header: '律师事务所', key: '律师事务所' },
+    { header: '会计师事务所', key: '会计师事务所' },
+    { header: '更新日期', key: '更新日期' },
+    { header: '受理日期', key: '受理日期' },
+    { header: '公司全称', key: '公司全称' },
+    { header: '公司简称', key: '公司简称' },
+    { header: '融资金额(亿元)', key: '融资金额(亿元)' },
+    { header: '保荐代表人', key: '保荐代表人' },
+    { header: '签字会计师', key: '签字会计师' },
+    { header: '签字律师', key: '签字律师' },
+    { header: '评估机构', key: '评估机构' },
+    { header: '签字评估师', key: '签字评估师' }
+  ];
+
+  console.log('开始添加数据');
+  // 开始添加数据
+  for(let i in enList) {
+    worksheet.addRow(enList[i]).commit();
+  }
+  console.log('添加数据完毕：');
+
+  workbook.commit();
+
+  const instance = axios.create({
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  instance.post('https://oapi.dingtalk.com/robot/send?access_token=70852b0831174729bb2bc6b2d00955816b4bc82fda078c41c6a0c0f31551fd60', {
+    msgtype: 'text',
+    text: {
+      "content": `爬虫执行完成，下载链接为：http://localhost:4000/${filename}`
+    }
+  })
 }
 
-app()
+router.get('/', async (ctx) => {
+  task()
+  ctx.body = '爬虫开始执行。。。执行结束后会发送钉钉通知。。。请不要反复刷新！'
+})
+
+app.use(router.routes())
+app.use(router.allowedMethods())
+app.use(KoaStatic(path.join( __dirname, './data')));
+
+app.listen(5566)
